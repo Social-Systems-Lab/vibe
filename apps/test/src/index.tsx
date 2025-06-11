0660055b646b22a08f4d9a36380a633c23af1daa
Patrik Opacic <opacic@gmail.com>
2025-06-12 00:24:18 +0200

    vibe-sdk and vibe-react

--- apps/test/src/index.tsx
+++ apps/test/src/index.tsx
@@ -30,7 +30,7 @@ const server = serve({
     },
 
     development: process.env.NODE_ENV !== "production",
-    port: 5000,
+    port: 5001,
 });
 
 console.log(`🚀 Server running at ${server.url}`);
