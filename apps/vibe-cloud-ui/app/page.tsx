447375ca2c0c9f9c9c8d03aff9b483054b3b51b7
Patrik Opacic <opacic@gmail.com>
2025-08-11 22:21:29 +0200

    vibe-cloud-ui deploy

--- apps/vibe-cloud-ui/app/page.tsx
+++ apps/vibe-cloud-ui/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
-    const appVersion = process.env.APP_VERSION || "local";
+    const appVersion = process.env.NEXT_PUBLIC_APP_VERSION || "local";
     console.log(`Rendering Vibe Cloud UI version: ${appVersion}`);
     return <h1 className="font-heading">Vibe Cloud UI version: {appVersion}</h1>;
 }
