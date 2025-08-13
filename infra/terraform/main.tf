b5c33c1d2b01652ebfe974712e11ac699d8f7717
Patrik Opacic <opacic@gmail.com>
2025-08-13 11:27:23 +0200

    terraform fix

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -3,14 +3,11 @@ terraform {
     bucket                      = "vibe-terraform-state" # This bucket must be created manually
     key                         = "terraform.tfstate"
     region                      = "fr-par"
-    endpoints = {
-      s3 = "https://s3.fr-par.scw.cloud"
-    }
+    endpoint                    = "https://s3.fr-par.scw.cloud"
     skip_credentials_validation = true
     skip_region_validation      = true
-    skip_requesting_account_id  = true
     skip_metadata_api_check     = true
-    use_path_style              = true
+    force_path_style            = true
   }
   required_providers {
     scaleway = {
