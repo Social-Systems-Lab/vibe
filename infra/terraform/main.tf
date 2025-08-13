3d1d78d3a485921308e245f69bce551b5a64d70a
Patrik Opacic <opacic@gmail.com>
2025-08-13 11:22:44 +0200

    terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
     skip_region_validation      = true
     skip_requesting_account_id  = true
     skip_metadata_api_check     = true
-    use_path_style           = true
+    use_path_style              = true
   }
   required_providers {
     scaleway = {
